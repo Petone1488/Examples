e2320d355f032d79a133fa442e1a92e585be5ecb
Petone1488 <main.mailbox@bk.ru>
2023-02-24 12:12:16 +0300

    Giri

---
+++ Example006_GIRI/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 9;
+int b = 2;
+int c = 6;
+int d = 8;
+int e = 3;
+int min = a;
+if(b < min) min = b;
+if(c < min) min = c;
+if(d < min) min = d;
+if(e < min) min = e;
+
+Console.Write("min = ");
+Console.WriteLine(min);
+
+
+
