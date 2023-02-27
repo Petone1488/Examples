65f0a39a49ce80fbe2cc23d364fd7faf29390ac8
Petone1488 <main.mailbox@bk.ru>
2023-02-27 15:42:55 +0300

    Example 011

--- Example011/Program.cs
+++ Example011/Program.cs
@@ -1,6 +1,4 @@
-﻿int[] array = new int[10];
-
-void FillArray(int[] collection)
+﻿void FillArray(int[] collection)
 {
     int lenght = collection.Length;
     int index = 0;
@@ -22,5 +20,35 @@ void PrintArray(int[] col)
     }
 }
 
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = 0;
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+
 FillArray(array);
+
+array[3] = 4;
+array[2] = 4;
+
+
 PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 5);
+Console.WriteLine(pos);
+
+
