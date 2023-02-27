5e9b6ba1a03fd63d7583538710aa0ae3a454ccf7
Petone1488 <main.mailbox@bk.ru>
2023-02-27 11:44:57 +0300

    example 010 method array

---
+++ Example010/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = { 1, 2, 3, 7, 9, 87, 90, 99 };
+
+int n = array.Length;
+int find = 90;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    // index = index + 1;
+    index++;
+}
